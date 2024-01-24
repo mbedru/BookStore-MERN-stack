d36591314cc79c1f171b9c53d004e7dd4591d8be
mbedru <34227943+mbedru@users.noreply.github.com>
2024-01-24 10:21:42 +0300

    Update config.js  remove mongodb u r i for company security reason

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5557;
-export const mongoDB_URI = 'mongodb+srv://mbedru19:abc12345@cluster0.7i5mar9.mongodb.net/books-store?retryWrites=true&w=majority';
+export const mongoDB_ U R I = 'your mongodb u r l';
